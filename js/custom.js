0c7493d03172b0bce07f206791336688c982e925
dailtonccomp <112089713+dailtonccomp@users.noreply.github.com>
2022-12-14 09:10:06 -0300

    subindo segundo jair

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+for(var i = 1; i<=26; i++) {    
+    
+        document.getElementById("imagem1").innerHTML += "<a href='img/periodo1/"+i+".jpg' data-lightbox='mygallery' data-title='1º período'><img src='img/periodo1/"+i+".jpg' alt='Foto "+i+"'></a>";
+}
+
+for(var i = 1; i<=100; i++) {    
+    
+    document.getElementById("imagem2").innerHTML += "<a href='img/periodo2/"+i+".jpg' data-lightbox='mygallery' data-title='2º período'><img src='img/periodo2/"+i+".jpg' alt='Foto "+i+"'></a>";
+}
+
+for(var i = 1; i<=22; i++) {    
+    
+    document.getElementById("imagem3").innerHTML += "<a href='img/africa/"+i+".jpg' data-lightbox='mygallery' data-title='II Edição do Projeto Africanidades'><img src='img/africa/"+i+".jpg' alt='Foto "+i+"'></a>";
+}
+
