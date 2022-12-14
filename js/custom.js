6993eefb40a34540a862f4f8f2f64467eec70aa8
dailtonccomp <112089713+dailtonccomp@users.noreply.github.com>
2022-12-14 09:32:37 -0300

    atualizando

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,10 @@ for(var i = 1; i<=26; i++) {
         document.getElementById("imagem1").innerHTML += "<a href='img/periodo1/"+i+".jpg' data-lightbox='mygallery' data-title='1º período'><img src='img/periodo1/"+i+".jpg' alt='Foto "+i+"'></a>";
 }
 
-for(var i = 1; i<=100; i++) {    
+// for(var i = 1; i<=100; i++) {    
     
-    document.getElementById("imagem2").innerHTML += "<a href='img/periodo2/"+i+".jpg' data-lightbox='mygallery' data-title='2º período'><img src='img/periodo2/"+i+".jpg' alt='Foto "+i+"'></a>";
-}
+//     document.getElementById("imagem2").innerHTML += "<a href='img/periodo2/"+i+".jpg' data-lightbox='mygallery' data-title='2º período'><img src='img/periodo2/"+i+".jpg' alt='Foto "+i+"'></a>";
+// }
 
 for(var i = 1; i<=22; i++) {    
     
